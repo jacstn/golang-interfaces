f2c214c5189d174eaa57973d38d428f9f39a232b
Jacek Stanusz <jacek.stanusz@gmail.com>
2022-08-10 22:15:12 +0200

    Create interfaces.go

---
+++ interfaces.go
@@ -0,0 +1,30 @@
+package interfaces
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func (englishBot) getGreeting() string {
+	return "hello!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "hola!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
