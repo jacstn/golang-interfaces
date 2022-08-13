cb1174a6f0e7ca1e6acd846ee764fbf39e2b9947
Jacek Stan <jacek.stanusz@gmail.com>
2022-08-13 17:20:34 +0200

    project restructurization

--- simple-server/cmd/web/main.go
+++ simple-server/cmd/web/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/jacstn/golang-playground/simple-server/pkg/handlers"
 )
 
 const portNumber = ":3000"
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application %s", portNumber))
 	http.ListenAndServe(portNumber, nil)
---
+++ simple-server/go.mod
@@ -0,0 +1,3 @@
+module github.com/jacstn/golang-playground/simple-server
+
+go 1.19
