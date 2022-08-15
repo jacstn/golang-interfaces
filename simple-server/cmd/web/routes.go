4331be5d5f0fa02359fe7123e9515285529f1bc2
Jacek Stan <jacek.stanusz@gmail.com>
2022-08-15 18:21:38 +0200

    use handle panic - recover

--- simple-server/cmd/web/routes.go
+++ simple-server/cmd/web/routes.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/jacstn/golang-playground/simple-server/pkg/handlers"
 )
 
 func routes() *chi.Mux {
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", handlers.Home)
 	mux.Get("/about", handlers.About)
